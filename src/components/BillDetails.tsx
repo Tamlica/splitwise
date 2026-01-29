bf223201ca5212d10e536ff4c6e667569a81b480
Tamlica Muhaimin <78264913+Tamlica@users.noreply.github.com>
2026-01-29 14:10:43 +0700

    Updated BillDetails.tsx

--- src/components/BillDetails.tsx
+++ src/components/BillDetails.tsx
@@ -80,9 +80,11 @@ const BillDetails = () => {
       } catch (err) {
         // Fallback to copying URL
         copyToClipboard(shareUrl);
+        copyRichBillSummaryToClipboard(bill?.id, bill?.restaurant_name);
       }
     } else {
       copyToClipboard(shareUrl);
+      copyRichBillSummaryToClipboard(bill?.id, bill?.restaurant_name);
     }
   };
 
